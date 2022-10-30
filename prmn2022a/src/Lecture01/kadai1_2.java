f6fd7262cb26caf531e2af9dce3fb6e67de36065
b2200190 <b2200190.photon@chitose.ac.jp>
2022-10-30 14:02:07 +0900

    1-2

---
+++ prmn2022a/src/Lecture01/kadai1_2.java
@@ -0,0 +1,11 @@
+package Lecture01;
+
+import java.util.Scanner;
+
+public class kadai1_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int StudentNUmber = scanner.nextInt();
+        System.out.println("B" +" "+StudentNUmber +"Shota ANDO");
+    }
+}
